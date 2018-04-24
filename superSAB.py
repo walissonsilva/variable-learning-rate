f1ed72713d37c888ca165ae0e7c34a09f4ea9054
Walisson Silva <walissonsilva10@gmail.com>
2018-04-24 07:55:44 -0300

    Ajustes no SuperSAB

--- superSAB.py
+++ superSAB.py
@@ -3,7 +3,7 @@ import numpy as np  # Array do Python
 from math import sqrt, pi
 
 class SuperSAB(object):
-    def __init__(self, delta0=0.01, alpha=1, delta_max=50, delta_min=1e-6, epoch_max=10000, Ni=2, Nh=15, Ns=1):
+    def __init__(self, delta0=0.01, alpha=0.05, delta_max=50, delta_min=1e-7, epoch_max=3000, Ni=2, Nh=15, Ns=1):
         self.delta0 = delta0
         self.alpha = alpha
         self.delta_max = delta_max
@@ -29,7 +29,7 @@ class SuperSAB(object):
         self.Wkj = np.random.rand(self.Ns, self.Nh + 1) * self.Wini
         self.taWji = np.ones((self.Nh, self.Ni)) * self.delta0
         self.taWkj = np.ones((self.Ns, self.Nh + 1)) * self.delta0
-        grji_ant = grkj_ant = 0
+        grji_ant = grkj_ant = deltaji_ant = deltakj_ant = 0
 
         MSE = np.zeros(self.epoch_max)
         plt.ion()
@@ -39,7 +39,9 @@ class SuperSAB(object):
             z = np.zeros(self.N)
             E = np.zeros(self.N)
 
-            for i in xrange(self.N):
+            idc = np.random.permutation(self.N)
+
+            for i in idc:
                 xi = np.array([-1, self.X_train[i]]).reshape(1, -1)
                 netj = np.dot(self.Wji, xi.T)
                 yj = 1 / (1 + np.exp(-netj.T))
@@ -57,6 +59,8 @@ class SuperSAB(object):
             grkj = np.sign(gradkj)
 
             if epoca == 0:
+                deltakj = -self.delta0 * gradkj
+                deltaji = -self.delta0 * gradji
                 self.Wkj += (-self.delta0 * gradkj)
                 self.Wji += (-self.delta0 * gradji)
             else:
@@ -69,22 +73,27 @@ class SuperSAB(object):
                 for i in xrange(sizeji[0]):
                     for j in xrange(sizeji[1]):
                         if (Dji[i,j] > 0):
-                            self.taWji[i,j] = min(self.taWji[i,j] * self.eta_plus, self.delta_max)
+                            self.taWji[i,j] = min(self.taWji[i,j] * self.eta_plus, self.delta_max)#self.taWji[i,j] * self.eta_plus#min(self.taWji[i,j] * self.eta_plus, self.delta_max)
                         elif (Dji[i,j] < 0):
-                            self.taWji[i,j] = max(self.taWji[i,j] * self.eta_less, self.delta_min)
+                            self.taWji[i,j] = max(self.taWji[i,j] * self.eta_less, self.delta_min)#self.taWji[i,j] * self.eta_less#max(self.taWji[i,j] * self.eta_less, self.delta_min)
                 
                 for i in xrange(sizekj[0]):
                     for j in xrange(sizekj[1]):
                         if (Dkj[i,j] > 0):
-                            self.taWkj[i,j] = min(self.taWkj[i,j] * self.eta_plus, self.delta_max)#self.taWkj[i,j] * self.eta_plus#min(self.taWkj[i,j] * self.eta_plus, self.delta_max)
+                            self.taWkj[i,j] = min(self.taWkj[i,j] * self.eta_plus, self.delta_max)#self.taWkj[i,j] * self.eta_plus#self.taWkj[i,j] = min(self.taWkj[i,j] * self.eta_plus, self.delta_max)
                         elif (Dkj[i,j] < 0):
-                            self.taWkj[i,j] = max(self.taWkj[i,j] * self.eta_less, self.delta_min)#self.taWkj[i,j] * self.eta_less#max(self.taWkj[i,j] * self.eta_less, self.delta_min)
+                            self.taWkj[i,j] = max(self.taWkj[i,j] * self.eta_less, self.delta_min)#self.taWkj[i,j] * self.eta_less#self.taWkj[i,j] = max(self.taWkj[i,j] * self.eta_less, self.delta_min)
 
-                self.Wkj = (-self.taWkj * gradkj) + (self.alpha * self.Wkj)
-                self.Wji = (-self.taWji * gradji) + (self.alpha * self.Wji)
+                deltakj = (-self.taWkj * gradkj) + (self.alpha * deltakj_ant)
+                deltaji = (-self.taWji * gradji) + (self.alpha * deltaji_ant)
+                self.Wkj += deltakj
+                self.Wji += deltaji
             
             grji_ant = grji
             grkj_ant = grkj
+            deltaji_ant = deltaji
+            deltakj_ant = deltakj
+
 
             MSE[epoca] = np.sum(E) / self.N
 
@@ -103,7 +112,7 @@ class SuperSAB(object):
     def plot(self, saida, epoca):
         plt.figure(0)
         y, = plt.plot(self.X_train, saida, label="y")
-        d, = plt.plot(self.X_train, self.d, '*', label="d")
+        d, = plt.plot(self.X_train, self.d, '.', label="d")
         plt.legend([y, d], ['Output of Network Neural', 'Desired Value'])
         plt.xlabel('x')
         plt.ylabel('f(x)')
@@ -118,7 +127,7 @@ class SuperSAB(object):
         plt.title('Mean Square Error (MSE)')
         plt.xlabel('Training Epochs')
         plt.ylabel('MSE')
-        plt.plot(np.arange(0, MSE.size), MSE)
+        plt.semilogy(np.arange(0, MSE.size), MSE)
         plt.show()
 
     def show_function(self):
@@ -129,8 +138,8 @@ class SuperSAB(object):
         plt.plot(self.X_train, self.d)
         plt.show()
 
-#super_sab = SuperSAB()
+super_sab = SuperSAB()
 
-#super_sab.load_function()
-#MSE = super_sab.train()
-#super_sab.plot_MSE(MSE)
+super_sab.load_function()
+MSE = super_sab.train()
+super_sab.plot_MSE(MSE)
